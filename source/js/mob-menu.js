b547a41b15accff838eec265a7ba88d1644c8665
ToryaToria <toria.torya@yandex.ru>
2025-04-23 10:32:15 +0300

    мобильное меню

---
+++ source/js/mob-menu.js
@@ -0,0 +1,25 @@
+// console.log('ky');
+
+const body = document.body;
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const link = document.querySelectorAll('.nav__link');
+
+burger.addEventListener('click', () => {
+
+  burger.classList.toggle('burger--close');
+  burger.classList.toggle('burger--open');
+  nav.classList.toggle('nav--open');
+  body.classList.toggle('page__body--noscroll');
+})
+
+link.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    // console.log(e);
+    burger.classList.remove('burger--open');
+    burger.classList.add('burger--close');
+
+    nav.classList.add('nav--open');
+    body.classList.remove('page__body--noscroll');
+  })
+});
