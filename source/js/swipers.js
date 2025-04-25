aee6e2f1483cac2ac0440c1e6d80e4f81a1440ed
ToryaToria <toria.torya@yandex.ru>
2025-04-25 21:54:35 +0300

    слайдер adv - тест прошел в mob и descktop

--- source/js/swipers.js
+++ source/js/swipers.js
@@ -6,41 +6,41 @@ import 'swiper/css/pagination';
 
 console.log('ky');
 
-
-
 const swiperHero = new Swiper('.hero__swiper', {
-  modules: [Navigation, Pagination],
+  modules: [Pagination],
   loop: true, // зациклен
   allowTouchMove: true, // свайп и мышка
   direction: 'horizontal', // по умолчанию
+  grabCursor: true,
 
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
-    // кастомные свойства
-//     renderBullet: function (index, className) {
-// return '<span class="' + className + '">' + (index + 1) + '<span>';
-//      }
   },
 
   breakpoints: {
     1440: {
       simulateTouch: false,
+      grabCursor: false,
+
     }
   }
 });
 
-swiperHero.on('slideChange', () => {
-  console.log('slider change');
-  console.log(swiperHero.activeIndex);
-});
+// swiperHero.on('slideChange', () => {
+//   console.log('slider change');
+//   console.log(swiperHero.activeIndex);
+// });
 
 
 const swiperTours = new Swiper('.tours__swiper', {
+  modules: [Navigation],
+
   loop: false, // не зациклено
   allowTouchMove: true, // свайп и мышка
   spaceBetween: 15,
+  grabCursor: true,
 
   navigation: {
     nextEl: '.swiper-button-next-tours',
@@ -65,100 +65,173 @@ swiperTours.on('slideChange', () => {
   console.log(swiperTours.activeIndex);
 });
 
-const toursBtnNext = document.querySelector('.swiper-button-next-tours');
+// const toursBtnNext = document.querySelector('.swiper-button-next-tours');
 
-const toursBtnPrev = document.querySelector('.swiper-button-back-tours');
+// const toursBtnPrev = document.querySelector('.swiper-button-back-tours');
 
-toursBtnNext.addEventListener('click', () => {
-  swiperTours.slideNext();
-  console.log('вперед');
-});
+// toursBtnNext.addEventListener('click', () => {
+//   swiperTours.slideNext();
+//   console.log('вперед');
+// });
 
-toursBtnPrev.addEventListener('click', () => {
-  swiperTours.slidePrev();
-  console.log('назад');
-});
+// toursBtnPrev.addEventListener('click', () => {
+//   swiperTours.slidePrev();
+//   console.log('назад');
+// });
 
 const swiperTraining = new Swiper('.training__swiper', {
+  modules: [Navigation],
+
   loop: false, // не зациклено
   allowTouchMove: true, // свайп и мышка
   spaceBetween: 15,
+  grabCursor: true,
+  // centeredSlides: true,
+  initialSlide: 2,
+
+  navigation: {
+    nextEl: '.swiper-button-next-training',
+    prevEl: '.swiper-button-back-training',
+  },
 
   breakpoints: {
     768: {
       slidesPerView: 3,
       spaceBetween: 20,
+      initialSlide: 0,
+
 
     },
     1440: {
       slidesPerView: 4,
+      initialSlide: 0,
+      spaceBetween: 20,
+
     }
   }
 });
 
-swiperTraining.on('slideChange', () => {
-  console.log('slider change');
-  console.log(swiperTraining.activeIndex);
-});
+// swiperTraining.on('slideChange', () => {
+//   console.log('slider change');
+//   console.log(swiperTraining.activeIndex);
+// });
 
 const swiperReviews = new Swiper('.reviews__swiper', {
+  modules: [Navigation],
+
   loop: false, // не зациклено
   allowTouchMove: true, // свайп и мышка
   spaceBetween: 15,
 
+  navigation: {
+    nextEl: '.swiper-button-next-reviews',
+    prevEl: '.swiper-button-back-reviews',
+  },
+
   breakpoints: {
     768: {
       spaceBetween: 30,
+      slidesPerView: '1.2',
 
     },
     1440: {
       spaceBetween: 120,
+      slidesPerView: 'auto',
+
     }
   }
 
 });
 
-swiperReviews.on('slideChange', () => {
-  console.log('slider change');
-  console.log(swiperReviews.activeIndex);
-});
+// swiperReviews.on('slideChange', () => {
+//   console.log('slider change');
+//   console.log(swiperReviews.activeIndex);
+// });
 
 
-const swiperAdv = new Swiper('.adv__swiper', {
-  breakpoints: {
-    1440: {
-      loop: true, // зациклен
-      allowTouchMove: true, // свайп и мышка
-      spaceBetween: 30,
-    }
-  }
-});
+// window.addEventListener('resize', () => {
+//   console.log('обновили экран');
 
-swiperAdv.on('slideChange', () => {
-  console.log('slider change');
-  console.log(swiperAdv.activeIndex);
-});
 
-const swiperGallery = new Swiper('.gallery__swrapper', {
-       loop: true, // зациклен
-      allowTouchMove: true, // свайп и мышка
-      slidesPerView: 2,
-      spaceBetween: 5,
+const body = document.body;
+console.log(body.offsetWidth);
 
-      breakpoints: {
-        768: {
-          slidesPerView: 3,
-    
+  const swiperAdv = new Swiper('.adv__swiper', {
+    modules: [Navigation],
+  
+    enabled: false,
+    breakpoints: {
+      1440: {
+        enabled: true,
+        loop: true, // зациклен
+        allowTouchMove: true, // свайп и мышка
+        grabCursor: true,
+
+  //       initialSlide: 2,
+  // centeredSlides: true,
+
+
+        spaceBetween: 30,
+        slidesPerView: 'auto',
+        slidesPerGroup: 2,
+  // centeredSlides: true,
+  // loopAddBlankSlides: true,
+  // loopFillGroupWithBlank: true,
+  
+        navigation: {
+          nextEl: '.swiper-button-next-adv',
+          prevEl: '.swiper-button-back-adv',
         },
-        1440: {
-// КАК ОТКЛЮЧИТЬ СЛАЙДЕР?
-        }
-      }
     
-});
+  
+      }
+    }
+  });
 
-swiperGallery.on('slideChange', () => {
-  console.log('slider change');
-  console.log(swiperGallery.activeIndex);
-});
+  console.log(swiperAdv.slides.length);
+  const adv = document.querySelector('.adv__list');
+
+
+      // for (let i = 0; i < swiperAdv.slides.length; i++) {
+      //   const virtualSlide = swiperAdv.slides[i].cloneNode(true);
+      //   virtualSlide.style.backgroundColor = 'red';
+      //   adv.appendChild(virtualSlide);
+      // }
+
+
+  if(body.offsetWidth < 1440) {
+    swiperAdv.destroy();
+  }
+    
+
+// swiperAdv.on('click', () => {
+//   console.log('slider change');
+//   console.log(swiperAdv.activeIndex);
+//   adv.style.marginLeft = '0';
+// });
+
+
+
+// const swiperGallery = new Swiper('.gallery__swrapper', {
+//   loop: true, // зациклен
+//   allowTouchMove: true, // свайп и мышка
+//   slidesPerView: 2,
+//   spaceBetween: 5,
+
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 3,
+
+//     },
+//     1440: {
+//       // КАК ОТКЛЮЧИТЬ СЛАЙДЕР?
+//     }
+//   }
+
+// });
+
+// swiperGallery.on('slideChange', () => {
+//   console.log('slider change');
+//   console.log(swiperGallery.activeIndex);
+// });
 
